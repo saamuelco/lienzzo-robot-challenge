d3fe1bfa82427e47788e0de1d9ab9ab354f3ff75
saamuelco <samuelcaor.sc@gmail.com>
2026-01-14 19:35:34 +0100

    Tipos de datos necesarios para la lógica

---
+++ types/index.ts
@@ -0,0 +1,33 @@
+// types/index.ts
+
+// Direcciones posibles (Norte, Sur, Este, Oeste)
+export type Direction = 'N' | 'S' | 'E' | 'W';
+
+// Coordenadas X, Y
+export interface Coordinates {
+  x: number;
+  y: number;
+}
+
+// Estado básico del robot
+export interface RobotState extends Coordinates {
+  dir: Direction;
+}
+
+// Un paso individual en el log (para la animación posterior)
+export interface SimulationStep extends RobotState {
+  stepIndex: number;
+  action: 'START' | 'MOVE' | 'LEFT' | 'RIGHT' | 'INVALID_MOVE';
+}
+
+// La estructura completa de una simulación (lo que guardamos en Base de Datos)
+export interface Simulation {
+  id: string;
+  created_at: string;
+  commands: string;       
+  is_success: boolean;    
+  final_x: number;
+  final_y: number;
+  execution_log: SimulationStep[]; 
+  obstacles: Coordinates[];      
+}
